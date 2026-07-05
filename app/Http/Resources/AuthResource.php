f5643c329b4b732a20b679d2a1c03d7918ca910a
Abdullah Fathallah <abdullahxoff@gmail.com>
2026-07-05 22:07:54 +0300

    feat: add AuthResource for transforming authentication response data

---
+++ app/Http/Resources/AuthResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'token' => $this->resource['token'],
+            'user'  => [
+                'id'    => $this->resource['user']->id,
+                'name'  => $this->resource['user']->name,
+                'email' => $this->resource['user']->email,
+                'role'  => $this->resource['user']->role,
+            ],
+            'tenant' => [
+                'id'     => $this->resource['tenant']->id,
+                'name'   => $this->resource['tenant']->name,
+                'domain' => $this->resource['tenant']->domain,
+                'plan'   => $this->resource['tenant']->plan->name,
+            ],
+        ];
+    }
+}
