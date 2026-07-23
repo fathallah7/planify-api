4b7b4f29f6a5cfccc3df8e8a84a6c01a1a232f0f
Abdullah Fathallah <abdullahxoff@gmail.com>
2026-07-23 16:09:56 +0300

    feat: add subscription and webhook routes to API

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,10 @@
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\InvitationController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\SubscriptionController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\WebhookController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
@@ -20,6 +23,14 @@ Route::middleware(['auth:sanctum', 'tenant'])->group(function () {
     Route::apiResource('projects', ProjectController::class);
     Route::apiResource('projects.tasks', TaskController::class);
     Route::post('/invitations', [InvitationController::class, 'invite']);
+
+    Route::post('/subscribe', [SubscriptionController::class, 'subscribe']);
+    Route::post('/subscribe/cancel', [SubscriptionController::class, 'cancel']);
 });
 
+Route::get('/plans', [SubscriptionController::class, 'plans']);
+
 Route::get('/invitations/accept/{token}', [InvitationController::class, 'accept']);
+
+Route::post('/webhooks/stripe', [WebhookController::class, 'handle'])
+    ->withoutMiddleware([VerifyCsrfToken::class]);
